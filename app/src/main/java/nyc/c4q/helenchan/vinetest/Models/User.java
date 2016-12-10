181568fcfb59a1cbca5f9d364192435ee98c64e3
Helen Chan <hchan22@gmail.com>
2016-12-10 17:34:29 -0500

    fourth stepish

--- app/src/main/java/nyc/c4q/helenchan/vinetest/Models/User.java
+++ app/src/main/java/nyc/c4q/helenchan/vinetest/Models/User.java
@@ -12,13 +12,6 @@ import java.util.List;
 
 public class User {
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     @SerializedName("username")
     @Expose
